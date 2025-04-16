01056abaa20cb648aac38cc3f577df30d51bcf28
bobr-ik <aavramenko804@gmail.com>
2025-04-16 22:00:09 +0300

    changed person page

--- frontend/parser.php
+++ frontend/parser.php
@@ -1,4 +1,4 @@
-<?php
+<!-- <?php
 	$id = $_GET["id"];
 	$year = $_GET["year"];
 	echo('ID : ' . $id . '| YEAR : ' . $year);
@@ -6,4 +6,4 @@
 	$con_str= new mysqli ('localhost', 'root', '', 'mysql');
 	$result = $con_str -> query( "SELECT * FROM `mysql`.`table 11` WHERE id='$id'" );
 	echo($result);
-?>
+?> -->
