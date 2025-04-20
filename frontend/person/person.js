c49a01170e9485f287425897d0331d35430f999b
bobr-ik <aavramenko804@gmail.com>
2025-04-20 14:14:26 +0300

    initial version of profile

--- frontend/person/person.js
+++ frontend/person/person.js
@@ -21,33 +21,42 @@ const personData = {
     years: [
         {
             year: "1940",
-            story: "В 1940 году Иван Иванов был призван в армию...",
-            images: ["path/to/1940_image1.jpg", "path/to/1940_image2.jpg"]
+            story: "В 1940 году Иван Иванов был призван в армию...Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur natus perspiciatis aliquam distinctio ipsum deserunt doloribus doloremque voluptatem numquam adipisci earum iusto eos unde dolore laudantium, cupiditate blanditiis quibusdam rerum!Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur natus perspiciatis aliquam distinctio ipsum deserunt doloribus doloremque voluptatem numquam adipisci earum iusto eos unde dolore laudantium, cupiditate blanditiis quibusdam rerum!Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur natus perspiciatis aliquam distinctio ipsum deserunt doloribus doloremque voluptatem numquam adipisci earum iusto eos unde dolore laudantium, cupiditate blanditiis quibusdam rerum!Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur natus perspiciatis aliquam distinctio ipsum deserunt doloribus doloremque voluptatem numquam adipisci earum iusto eos unde dolore laudantium, cupiditate blanditiis quibusdam rerum!",
+            images: ["../src/photo.jpg", "../src/photo.jpg", "../src/photo.jpg", "../src/photo.jpg"],
+            location: [55.7558, 37.6173]
         },
         {
             year: "1941",
             story: "В 1941 году Иван участвовал в обороне Москвы...",
-            images: ["path/to/1941_image1.jpg"]
+            images: ["path/to/1941_image1.jpg", "../src/photo.jpg", "../src/phdoto.jpg"],
+            location: [55.7558, 37.6173]
+
         },
         {
             year: "1942",
             story: "В 1942 году Иван был ранен в бою под Сталинградом...",
-            images: ["path/to/1942_image1.jpg", "path/to/1942_image2.jpg"]
+            images: ["../src/photo.jpg"],
+            location: []
+
         },
         {
             year: "1943",
             story: "После госпиталя в 1943 году вернулся на фронт...",
-            images: ["path/to/1943_image1.jpg"]
+            images: [],
+            location: [55.7558, 37.6173]
+
         },
         {
             year: "1944",
             story: "В 1944 году участвовал в операции «Багратион»...",
-            images: ["path/to/1944_image1.jpg"]
+            images: ["path/to/1944_image1.jpg"],
+            location: [75.7558, 37.6173]
         },
         {
             year: "1945",
             story: "День Победы встретил в Берлине...",
-            images: ["path/to/1945_image1.jpg", "path/to/1945_image2.jpg", "path/to/1945_image3.jpg"]
+            images: ["path/to/1945_image1.jpg", "path/to/1945_image2.jpg", "path/to/1945_image3.jpg"],
+            location: [55.7558, 37.6173]
         }
     ],
 };
@@ -58,7 +67,7 @@ function createMap() {
         center   : [50.881176, 30.371177],
         zoom     : 5,
         maxZoom  : 100,
-        minZoom  : 4,
+        minZoom  : 2,
     })
 
     L.tileLayer('https://tile.jawg.io/jawg-dark/{z}/{x}/{y}{r}.png?access-token=jFVHTIg2WK3xJlt5wfqJ7F41zVI76aPnnsvSD3Pm4pjbR1J2mpnmUEMyUFjWKQY8', {}).addTo(map);
@@ -67,46 +76,130 @@ function createMap() {
 }
 
 function addMarker(map, location) {
-    //create icon
     let icon = L.icon({
-        iconUrl: './src/star.svg',
-        shadowUrl: './src/shadow.png',
+		iconUrl      : '../src/star.svg',
+		shadowUrl    : '../src/shadow.png',
 
-        iconSize: [30, 30],
-        iconAnchor: [15, 15],
+		iconSize     : [30,30],
+		iconAnchor   : [15,15],
 
-        shadowSize: [60, 60],
-        shadowAnchor: [30, 30],
+		shadowSize   : [60,60],
+		shadowAnchor : [30,30],
     })
-    if (info.location != false) {
-        info.location = info.location.split(" ").map(Number);
+    
+    if (location == false) {
+        document.getElementById('map').style.display = 'none';
+        document.getElementById('description_by_years').style.gridTemplateColumns = '1fr';
+        location = [0, 0];
+    }
+    else {
+        document.getElementById('map').style.display = 'block'
+        document.getElementById('description_by_years').style.gridTemplateColumns = '1fr 2fr';
+
     }
 
-    if (info.location == false) info.location = [0, 0];
-    
-    
-    //create marker
-    let marker = L.marker([info.location[0], info.location[1]], { icon: icon });
-    console.log(info.location)
-    
+	
+	//create marker
+	let marker = L.marker( [location[0], location[1]],{icon:icon} );
+	marker.addTo(map);
 
-    //connect with shadow
-    if (marker._icon) marker._icon.shadow = marker._shadow;
+	//connect with shadow
+	if(marker._icon) marker._icon.shadow = marker._shadow;
 
-    //hover function
-    setTimeout(() => {
-        if (marker._icon && marker._shadow) {
-            marker._icon.addEventListener("mouseenter", () => {
-                marker._shadow.classList.add('shadow-visible');
-            });
-            marker._icon.addEventListener("mouseleave", () => {
-                marker._shadow.classList.remove('shadow-visible');
-            });
-        }
-    }, 0);
+	//remove shadow
+	if(marker._shadow) {
+		marker._shadow.style.opacity    = '0';
+		marker._shadow.style.transition = '.8s';
+	}
 
-    marker.addTo(map);
+	setTimeout(() => {
+		if (marker._icon && marker._shadow) {
+			marker._icon.addEventListener("mouseenter", () => {
+				marker._shadow.classList.add('shadow-visible');
+			});
+			marker._icon.addEventListener("mouseleave", () => {
+				marker._shadow.classList.remove('shadow-visible');
+			});
+		}
+	}, 0);
+    return marker
 }
+
+
+
+
+
+
+function create_slider(images) {
+    const slider = document.getElementById('photo_slider');
+    const prevBtn = document.getElementById('prevBtn');
+    const nextBtn = document.getElementById('nextBtn');
+
+    slider.innerHTML = '';
+    if (images.length == 0) {
+        slider.style.display = 'none';
+        return;
+    }
+    slider.style.display = 'block';
+
+
+    // Добавляем изображения в слайдер
+    images.forEach(src => {
+        const img = document.createElement('img');
+        img.src = src;
+        img.classList.add('slider_item');
+        slider.appendChild(img);
+    });
+
+    let currentIndex = 0;
+
+    // Функция для обновления положения слайдера и скрытия стрелок
+    function updateSlider() {
+        const totalImages = images.length;
+
+        // Показываем/скрываем кнопки
+        prevBtn.classList.toggle('slider_control_hidden', currentIndex === 0);
+        nextBtn.classList.toggle('slider_control_hidden', currentIndex === totalImages - 1);
+
+        // Скрываем все изображения, кроме текущего
+        const imgs = slider.querySelectorAll('img');
+        imgs.forEach((img, index) => {
+            if (index !== currentIndex) {
+                img.style.display = 'none';
+            } else {
+                img.style.display = 'block';
+            }
+        });
+    }
+
+    // Функция для перехода к следующему изображению
+    function nextSlide() {
+        if (currentIndex < images.length - 1) {
+            currentIndex++;
+            updateSlider();
+        }
+    }
+
+    // Функция для перехода к предыдущему изображению
+    function prevSlide() {
+        if (currentIndex > 0) {
+            currentIndex--;
+            updateSlider();
+        }
+    }
+
+    // Инициализация слайдера
+    updateSlider();
+
+    // Обработчики событий для кнопок
+    nextBtn.addEventListener('click', nextSlide);
+    prevBtn.addEventListener('click', prevSlide);
+
+}
+
+
+
+
 
 function initPage() {
     const { year_from_url } = getUrlParams();
@@ -126,21 +219,37 @@ function initPage() {
     rewards_elem.textContent = personData.rewards;
 
     const map = createMap();  
+    year_buttons = []
 
     personData.years.forEach(year => {
         const year_button = document.createElement('button');
         year_button.textContent = year.year;
+        year_button.classList.add('year_button');
+        year_buttons.push(year_button);
 
-        console.log(year.year, year_from_url);
+
         if (year.year === year_from_url) {
-            addMarker(map, year.location);
+            activated = year_button
+            marker = addMarker(map, year.location);
+            map.setView( [ year.location[0], year.location[1] ] );
             year_button.classList.add('active');
             year_description_elem.textContent = year.story;
-            photo_slider_elem.innerHTML = year.images.map(image => `<img class="img-fluid" src="${image}">`).join('');
+            create_slider(year.images);
         }
         year_button.addEventListener('click', () => {
+            console.log(year)
+            year_button.classList.add('active');
+            year_buttons.forEach(button => {
+                if (button !== year_button) {
+                    button.classList.remove('active');
+                }
+            })
+            marker.remove();
+            marker = addMarker(map, year.location);
+            if (year.location != false) map.setView( [ year.location[0], year.location[1] ] );
             year_description_elem.textContent = year.story;
-            photo_slider_elem.innerHTML = year.images.map(image => `<img class="img-fluid" src="${image}">`).join(''); 
+            photo_slider_elem.innerHTML = '';
+            create_slider(year.images);
         });
         timeline_elem.appendChild(year_button);
     });
