20319f95ac4e16910a4a05d6399f6acd14e8b6a4
bobr-ik <aavramenko804@gmail.com>
2025-05-04 12:02:36 +0300

    server commit

--- frontend/JS/search.js
+++ frontend/JS/search.js
@@ -3,11 +3,11 @@ async function get_points() {
 	year_from_url = url_params.get('year')
 
 	year = year_from_url ? Math.min(Math.max(parseInt(year_from_url, 10), 1940), 1945) : 1940
-	res = await fetch('http://127.0.0.1:8000/get_points?year=' + year);
-	data = await res.json(); 
+	res = await fetch('http://pummemory.pumibari.ru/api/get_points?year=' + year);
+	data = await res.json();
 	console.log(data);
 	return data
-	
+
 }
 
 //TODO
@@ -19,35 +19,35 @@ async function init_search() {
 	data = await get_points(year)
 
 	function getNames(data) {
-		return data.map(function(e) {
-							return {
-								name : e.name+" "+e.surname+" "+e.patronymic,
-								c    : -1,
-							}
-						})
+		return data.map(function (e) {
+			return {
+				name: e.name + " " + e.surname + " " + e.patronymic,
+				c: -1,
+			}
+		})
 	}
 
-	function style_name( obj, j ) {
-		if( obj.c>-1 ) {
-			name     = obj.name;
-			obj.name = name.substr(0,obj.c)+"<b>"+j+"</b>"+name.substr(obj.c+j.length);
+	function style_name(obj, j) {
+		if (obj.c > -1) {
+			name = obj.name;
+			obj.name = name.substr(0, obj.c) + "<b>" + j + "</b>" + name.substr(obj.c + j.length);
 		}
 		return obj
 	}
 
-	function searching( text, list ) {
-		if( text ) {
-			let names = list.map( function(x,y) { 
-									return style_name({ 
-											name:x.name,
-											c:x.name.indexOf(text)
-										}, text)
-								})
+	function searching(text, list) {
+		if (text) {
+			let names = list.map(function (x, y) {
+				return style_name({
+					name: x.name,
+					c: x.name.indexOf(text)
+				}, text)
+			})
 			names.sort(function (a, b) {
-				if (a.c ==-1 )  return  1;
-				if (b.c ==-1 )  return -1;
-				if (a.c > b.c)  return  1;
-				if (a.c < b.c)  return -1;
+				if (a.c == -1) return 1;
+				if (b.c == -1) return -1;
+				if (a.c > b.c) return 1;
+				if (a.c < b.c) return -1;
 				return 0;
 			});
 			return names;
@@ -57,20 +57,20 @@ async function init_search() {
 
 	let name_list = getNames(data);
 
-	$('.search-txt').on('input',function() {
+	$('.search-txt').on('input', function () {
 		let local_name_list = name_list;
-		let val             = $(this).val().trim();
-		let val_list        = val.split(" ").map(function(x,y) { return x.charAt(0).toUpperCase() + x.substr(1).toLowerCase()});
-		
-		for( let i in val_list ) { local_name_list = searching( val_list[i],local_name_list ); }
+		let val = $(this).val().trim();
+		let val_list = val.split(" ").map(function (x, y) { return x.charAt(0).toUpperCase() + x.substr(1).toLowerCase() });
 
-		if( local_name_list ) {
+		for (let i in val_list) { local_name_list = searching(val_list[i], local_name_list); }
+
+		if (local_name_list) {
 			$('.hints')[0].style.display = 'block';
 			let li_list = $('.hints ul li');
-			for( let i in local_name_list ) {
+			for (let i in local_name_list) {
 				li_list[i].innerHTML = local_name_list[i].name;
-				if( li_list[i].innerText  ) {
-					li_list[i].mark = mark_list_2[li_list[i].innerText.replace(' ','_').replace(' ','_')];
+				if (li_list[i].innerText) {
+					li_list[i].mark = mark_list_2[li_list[i].innerText.replace(' ', '_').replace(' ', '_')];
 				}
 			}
 		} else $('.hints')[0].style.display = 'none';
